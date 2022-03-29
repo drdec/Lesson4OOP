848b0aecdc24684e128a90763533feded7b95081
DrDeckto <fynjy138@gmail.com>
2022-03-30 01:52:02 +0300

    Выполнен первый пункт дз

---
+++ Lesson4/GeneratorId.cs
@@ -0,0 +1,16 @@
+﻿namespace Lesson4
+{
+    public static class GeneratorId
+    {
+        private static int _id;
+
+        /// <summary>
+        /// Возвращает уникальный номер
+        /// </summary>
+        /// <returns>Id</returns>
+        public static int GetId()
+        {
+            return ++_id;
+        }
+    }
+}
--- Lesson4/Program.cs
+++ Lesson4/Program.cs
@@ -1,9 +1,17 @@
-﻿namespace Lesson4
+﻿using System;
+
+namespace Lesson4
 {
     public class Program
     {
         public static void Main()
         {
+            var building = new Building();
+            var buildingTwo = new Building();
+
+            building.ShowInfo();
+            Console.WriteLine();
+            buildingTwo.ShowInfo();
 
         }
     }
